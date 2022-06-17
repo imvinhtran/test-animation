15e30f516b7d9f4b9224bd8ce3cdc6873c7aba42
vquang19 <vquanng1.9@gmail.com>
2022-06-18 03:18:14 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets10.lottiefiles.com/packages/lf20_edoysgy6.json',
+});
